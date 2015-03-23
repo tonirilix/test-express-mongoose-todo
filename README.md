c8414701e93cf7daa2152d3e2b71532894bae0d2
tonirilix <jesusmontes.mx@hotmail.com>
2015-03-23 17:05:06 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Express Todo Example
+
+Source code for the tutorial Write a todo list with Express and MongoDB
---
+++ app.js
@@ -0,0 +1,45 @@
+/**
+ * Module dependencies.
+ */
+
+// mongoose setup
+require( './db' );
+
+var express = require( 'express' );
+var routes  = require( './routes' );
+var http    = require( 'http' );
+var path    = require( 'path' );
+var app     = express();
+var engine  = require( 'ejs-locals' );
+
+// all environments
+app.set( 'port', process.env.PORT || 3001 );
+app.engine( 'ejs', engine );
+app.set( 'views', path.join( __dirname, 'views' ));
+app.set( 'view engine', 'ejs' );
+app.use( express.favicon());
+app.use( express.logger( 'dev' ));
+app.use( express.cookieParser());
+app.use( express.bodyParser());
+app.use( express.json());
+app.use( express.urlencoded());
+app.use( express.methodOverride());
+app.use( routes.current_user );
+app.use( app.router );
+app.use( express.static( path.join( __dirname, 'public' )));
+
+// development only
+if( 'development' == app.get( 'env' )){
+  app.use( express.errorHandler());
+}
+
+// Routes
+app.get(  '/',            routes.index );
+app.post( '/create',      routes.create );
+app.get(  '/destroy/:id', routes.destroy );
+app.get(  '/edit/:id',    routes.edit );
+app.post( '/update/:id',  routes.update );
+
+http.createServer( app ).listen( app.get( 'port' ), function (){
+  console.log( 'Express server listening on port ' + app.get( 'port' ));
+});
